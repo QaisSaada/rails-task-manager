f76241989798dd90634c111de2baa8355c0224da
Qais Saada <qais.saada@karnovgroup.com>
2022-09-15 09:20:36 +0200

    complete crude no frontend no testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'tasks#index'
-  get "tasks", to: "tasks#index"
+  # get 'tasks', to: 'tasks#index'
 
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
 
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 
 end
